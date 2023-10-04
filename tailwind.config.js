48869e3cd8ea5dad9f77b5e7c90416a48488dc76
Avinash Bagde <bagde.avinash66@gmail.com>
2023-10-04 14:45:03 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primaryColor: '#003974',
+        primary_md: '#2d639e',
+        secondaryColor: '#53b7e8',
+        primary_bg: '#fcfcfc',
+        lightgray: '#ebebec',
+        darkgray: '#9fa1a4',
+        mediumgray: '#cdcfd0',
+      },
+      animation: {
+        'spin-slow': 'spin 3s infinite',
+      },
+      borderRadius: {
+        '5': '5px',
+      },
+    },
+    container: {
+      padding: {
+        sm: '1rem',
+        lg: '1rem',
+        xl: '1rem',
+        '2xl': '1rem',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
